f7ded27144b7cfa6ba52ec2e2512c1378dc2e7f6
Szymon <szymqwerty@gmail.com>
2020-02-10 23:14:07 +0100

    added working version of dnd in react - pure solution without libraries

--- dnd-react/src/App.js
+++ dnd-react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const style = {
@@ -19,7 +19,55 @@ const style = {
   }
 };
 
+const initialElements = [
+  { name: 'Element 1', col: 3 },
+  { name: 'Element 2', col: 3 },
+  { name: 'Element 3', col: 3 }
+];
+
 function App() {
+  const [elements, setElements] = useState(initialElements);
+  const [draggedEl, setDraggedEl] = useState(null);
+
+  const handleDragStart = (e) => {
+    // debugger;
+    const target = e.target;
+    setDraggedEl(e.target.id);
+    setTimeout(() => {
+      target.style.display = 'none';
+    }, 0);
+  };
+  const handleDragEnd = (e) => {
+    const target = e.target;
+    setDraggedEl(null);
+
+    setTimeout(() => {
+      target.style.display = '';
+    }, 0);
+  };
+  const handleDragOver = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+  const handleDragEnter = (e) => {
+    e.currentTarget.style.backgroundColor = 'rgb(127, 248, 255)';
+  };
+
+  const handleDragLeave = (e) => {
+    e.currentTarget.style.backgroundColor = 'rgb(97, 218, 251)';
+  };
+
+  const handleDrop = (e) => {
+    if (['col1', 'col2', 'col3'].includes(e.target.id)) {
+      const colNum = parseFloat(e.target.id.split('col')[1]);
+      setElements([
+        ...elements.filter((el) => el.name !== draggedEl),
+        { name: draggedEl, col: colNum }
+      ]);
+    }
+    e.currentTarget.style.backgroundColor = 'rgb(97, 218, 251)';
+  };
+
   return (
     <div
       style={{
@@ -40,13 +88,81 @@ function App() {
         </h1>
       </header>
       <div style={style.listContainer}>
-        <div style={style.listStyle}>
-          <div draggable={true} style={style.listItem}>
-            Element 1
-          </div>
+        <div
+          id="col1"
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          style={style.listStyle}
+        >
+          {elements
+            .filter((el) => el.col === 1)
+            .map((el) => {
+              return (
+                <div
+                  key={el.name}
+                  id={el.name}
+                  onDragStart={handleDragStart}
+                  onDragEnd={handleDragEnd}
+                  draggable={true}
+                  style={style.listItem}
+                >
+                  {el.name}
+                </div>
+              );
+            })}
         </div>
-        <div style={style.listStyle}></div>
-        <div style={style.listStyle}></div>
+        <div
+          id="col2"
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          style={style.listStyle}
+        >
+          {elements
+            .filter((el) => el.col === 2)
+            .map((el) => {
+              return (
+                <div
+                  key={el.name}
+                  id={el.name}
+                  onDragStart={handleDragStart}
+                  onDragEnd={handleDragEnd}
+                  draggable={true}
+                  style={style.listItem}
+                >
+                  {el.name}
+                </div>
+              );
+            })}
+        </div>
+        <div
+          id="col3"
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          style={style.listStyle}
+        >
+          {elements
+            .filter((el) => el.col === 3)
+            .map((el) => {
+              return (
+                <div
+                  key={el.name}
+                  id={el.name}
+                  onDragStart={handleDragStart}
+                  onDragEnd={handleDragEnd}
+                  draggable={true}
+                  style={style.listItem}
+                >
+                  {el.name}
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
